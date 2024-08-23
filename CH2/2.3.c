475e5888a92e2081471ca02f9f81d420b38370e4
Nikolozi NEPARIDZE <nikolozi.neparidze@guarani.fr>
2024-08-23 09:24:48 +0200

    chap 2 2 exercices

---
+++ CH2/2.3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void squeeze(char *s, char *s2)
+{
+    int i, j, k;
+    int found;
+
+    // Iterate over each character in s
+    for (i = j = 0; s[i] != '\0'; i++)
+    {
+        // Assume the character is not found in s2
+        found = 0;
+
+        // Check if the character s[i] is in s2
+        for (k = 0; s2[k] != '\0'; k++)
+        {
+            if (s[i] == s2[k])
+            {
+                found = 1;
+                break; // No need to check further, character is found
+            }
+        }
+
+        // If the character is not found in s2, include it in the result
+        if (!found)
+        {
+            s[j++] = s[i];
+        }
+    }
+
+    // Null-terminate the resulting string
+    s[j] = '\0';
+}
+
+int main(void)
+{
+    char s1[] = "example";
+    char s2[] = "ae";
+    squeeze(s1, s2);
+    printf("Resulting string: %s\n", s1); // Output should be "xmpl"
+    return 0;
+}
---
+++ CH2/squeeze.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main(void)
+{
+    
+}
+
+
+
+void squeeze(s, c) 
+char s[];
+int c;
+{
+  int i,  j;
+
+  for (i = j = 0; s[i] != '\0'; i++)
+        if (s[i] != c)
+            s[j++] = s[i];
+  s[j] = '\0';
+}
